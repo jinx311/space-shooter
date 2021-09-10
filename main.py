0c38531655351e43937b1540eae7b16513f22929
astrid iliefski <astridiliefski@gmail.com>
2021-09-10 18:51:13 +0200

    added wasd controls

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ def main():
     pygame.display.set_caption("minimal program")
      
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((240,180))
+    screen = pygame.display.set_mode((500,500))
      
     # define a variable to control the main loop
     running = True
      
+    x= 100
+    y= 100
     # main loop
     while running:
         # event handling, gets all event from the event queue
@@ -25,7 +27,29 @@ def main():
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
-     
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_w]:
+                y=y-10
+                print("w is pressed")
+            if pressed[pygame.K_s]:
+                y=y+10
+                print("s is pressed")
+            if pressed[pygame.K_a]:
+                x=x-10
+                print("a is pressed")
+            if pressed[pygame.K_d]:
+                x=x+10
+                print("d is pressed")
+        
+        
+
+
+        screen.fill((255,255,255))
+        pygame.draw.circle(screen, (0,255,0), (x,y), 75)
+
+
+        # should be after all visual updates in loop. redraws screen
+        pygame.display.flip()    
      
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
